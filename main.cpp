5506b4ac253b554348313633fdca9f259731859a
luciano <luciano.geronimo.fnord@gmail.com>
2018-01-02 09:55:00 -0200

    Telinha dummy criada mas os eventos tão bugados (janela congelando)

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@
 #include <vtkSmartPointer.h>
 #include <vtkImageImport.h>
 #include "loadVolume.h"
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
 
 using namespace std;
+
+vtkSmartPointer<vtkRenderWindow> CreateDummyScreen();
 
 int main(int argc, char** argv){
 	//Carrega a imagem.
@@ -14,5 +19,17 @@ int main(int argc, char** argv){
 	std::map<std::string, std::string> metadataDaImagem;
 	itk::Image<short, 3>::Pointer imagemItk = LoadVolume(metadataDaImagem, lst);
 	vtkSmartPointer<vtkImageImport> imagemVtk = CreateVTKImage(imagemItk);
+
+	vtkSmartPointer<vtkRenderWindow> dummyWindow = CreateDummyScreen();
+	dummyWindow->GetInteractor()->Start();
 	return EXIT_SUCCESS;
+}
+
+vtkSmartPointer<vtkRenderWindow> CreateDummyScreen(){
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	renderWindow->SetInteractor(renderWindowInteractor);
+	return renderWindow;
 }
