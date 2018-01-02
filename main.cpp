a2fc9e1ff39238c8c09a85a29cd7de1bfff3a20c
luciano <luciano.geronimo.fnord@gmail.com>
2018-01-02 10:05:21 -0200

    Por enquanto o bug maluco da IDE não reapareceu.

--- main.cpp
+++ main.cpp
@@ -2,16 +2,42 @@
 #include <map>
 #include <string>
 #include <itkImage.h>
+#include <vtkImageData.h>
 #include <vtkSmartPointer.h>
 #include <vtkImageImport.h>
 #include "loadVolume.h"
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <memory>
+#include <vtkResliceImageViewer.h>
+#include <array>
 
 using namespace std;
 
 vtkSmartPointer<vtkRenderWindow> CreateDummyScreen();
+
+class IMPRView{
+public:
+	virtual vtkSmartPointer<vtkResliceImageViewer> GetVtkResliceImageViewer() = 0;
+	virtual void Atualizar() = 0;
+	virtual ~IMPRView(){};
+};
+
+class MPRView : public IMPRView{
+public:
+	vtkSmartPointer<vtkResliceImageViewer> GetVtkResliceImageViewer();
+	void Atualizar();
+	~MPRView(){};
+};
+
+class Sistema{
+private:
+	array<unique_ptr<MPRView>, 3> mprs;
+	vtkSmartPointer<vtkImageData> imagem;
+public:
+	Sistema(vtkSmartPointer<vtkImageData> img);
+};
 
 int main(int argc, char** argv){
 	//Carrega a imagem.
@@ -19,10 +45,30 @@ int main(int argc, char** argv){
 	std::map<std::string, std::string> metadataDaImagem;
 	itk::Image<short, 3>::Pointer imagemItk = LoadVolume(metadataDaImagem, lst);
 	vtkSmartPointer<vtkImageImport> imagemVtk = CreateVTKImage(imagemItk);
-
+	//Cria o sistema
+	unique_ptr<Sistema> sistema = make_unique<Sistema>(imagemVtk->GetOutput());
 	vtkSmartPointer<vtkRenderWindow> dummyWindow = CreateDummyScreen();
 	dummyWindow->GetInteractor()->Start();
 	return EXIT_SUCCESS;
+}
+
+vtkSmartPointer<vtkResliceImageViewer> MPRView::GetVtkResliceImageViewer(){
+	cout << __FUNCTION__ << endl;
+	return nullptr;
+}
+
+void MPRView::Atualizar(){
+	cout << __FUNCTION__ << endl;
+}
+
+
+Sistema::Sistema(vtkSmartPointer<vtkImageData> img){
+	imagem = img;
+	mprs[0] = make_unique<MPRView>();
+	mprs[1] = make_unique<MPRView>();
+	mprs[2] = make_unique<MPRView>();
+	mprs[0]->Atualizar();
+	void* v = mprs[0]->GetVtkResliceImageViewer();
 }
 
 vtkSmartPointer<vtkRenderWindow> CreateDummyScreen(){
